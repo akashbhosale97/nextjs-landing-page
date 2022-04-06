40532d4682d78cebd96c468077543a080fee188e
Akash Bhosale <MY_NAME@example.com>
2022-04-06 19:19:14 +0530

    ...

--- components/Nav.js
+++ components/Nav.js
@@ -1,26 +1,47 @@
 import React from "react";
-import styles from "../styles/Nav.module.scss";
 import Link from "next/link";
 
 const Nav = () => {
   return (
     <nav
-      className={`${styles.nav} flex-lg-row flex-column container-fluid fixed-top`}
+      class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top px-5"
+      style={{ height: "80px" }}
     >
-      <h1>
-        <Link href="/">DroidTech</Link>
-      </h1>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
+      <div class="container-fluid">
+        <a class="navbar-brand fs-2" href="#">
+          DroidTech
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
